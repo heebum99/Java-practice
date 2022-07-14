a529bf3532c5dfd56e1d6ada00bc1e84ec49924f
heebum99 <heebum9955@naver.com>
2022-07-14 23:30:00 +0900

    변수 선언, 개념

---
+++ src/Java100_variable_001.java
@@ -0,0 +1,21 @@
+public class Java100_variable_001 {
+    public static void main(String [] args){
+
+        int a;
+        int b;
+        int sum;
+
+        a=3;
+        b=4;
+        sum=a+b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(3+4);
+        System.out.println(sum);
+
+        sum=a+b+a;
+        System.out.println(sum);
+
+    }
+}
