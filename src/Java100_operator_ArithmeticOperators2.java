586d0694cba630b651887e99cbd69340bae71d3e
heebum99 <heebum9955@naver.com>
2022-07-16 19:51:37 +0900

    연산자 축약(++,+=...)

---
+++ src/Java100_operator_ArithmeticOperators2.java
@@ -0,0 +1,21 @@
+public class Java100_operator_ArithmeticOperators2 {
+    public static void main(String[]args){
+
+        int a=0, b=100;
+        a=a+1;
+        System.out.println(a);
+
+        //축약된 형태
+        a+=1; //a=a+1을 축약한 형태
+        System.out.println(a);
+
+        a-=1; //a=a-1을 축약한 형태
+        System.out.println(a);
+
+        b++; //b=b+1을 축약한 형태
+        System.out.println(b);
+
+        b--; //b=b-1을 축약한 형태
+        System.out.println(b);
+    }
+}
