7438c254ef29ce07df43a77c99b7655b5a59093e
heebum99 <heebum9955@naver.com>
2022-07-19 16:10:53 +0900

    반복문(for)

---
+++ src/Java100_for_Iteration001.java
@@ -0,0 +1,10 @@
+public class Java100_for_Iteration001 {
+    public static void main(String[] args) {
+
+        //for 반복문
+        // 실행 순서: 초기식 -> 조건식 -> 블록 -> 증감식 -> 조건식 (계속 반복)
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
