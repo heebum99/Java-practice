6f27533e846f05a51ba4b70b093a95cfe2067270
heebum99 <heebum9955@naver.com>
2022-07-19 16:15:58 +0900

    반복문(for)을 이용해 알파벳 출력(아스키 코드 활용)

---
+++ src/Java100_for_AlphabetPrint.java
@@ -0,0 +1,9 @@
+public class Java100_for_AlphabetPrint {
+    public static void main(String[] args) {
+
+        //for 반복문 => 아스키 코드로 A(65)~Z(90) 출력
+        for(int i=65;i<91;i++){
+            System.out.println((char)i); //정수를 문자로 형변환 후 출력
+        }
+    }
+}
