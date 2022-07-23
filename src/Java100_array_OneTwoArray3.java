0ff50bfa9702c73b347529329a481f36dcf40a2f
heebum99 <heebum9955@naver.com>
2022-07-23 13:54:52 +0900

    charAt()메서드, .length vs .length()의 차이점

--- src/Java100_array_OneTwoArray3.java
+++ src/Java100_array_OneTwoArray3.java
@@ -20,5 +20,42 @@ public class Java100_array_OneTwoArray3 {
         System.out.println(strAr[0].charAt(2)); //hong의 인덱스 2번째 값 출력 => n
         System.out.println(strAr[1].charAt(2)); //m
         System.out.println(strAr[2].charAt(2)); //r
+
+        //반복문에서 charAt()메서드 사용
+        //잘못된 예1
+        for(int i=0;i<strAr.length;i++){
+            System.out.print(strAr[i].charAt(i)); //hir
+        }
+        System.out.println();
+
+        //잘못된 예2
+        for(int k=0;k<strAr.length;k++){
+            for(int j=0;j< strAr.length;j++){
+                System.out.print(strAr[k].charAt(j)); //hon kim par
+            }
+            System.out.println();
+        }
+
+        //.length vs .length() 차이점
+        //.length => 배열의 길이, .length() => 문자열의 길이
+        System.out.println(strAr.length); //3
+        //System.out.println(strAr[0].length); => 에러 발생
+        System.out.println(strAr[0].length()); // 4 => h o n g
+
+        int [] aaa = new int [10];
+        System.out.println(aaa.length); //10 => 배열의 길이
+
+        String bbb = "Welcome to Seoul";
+        System.out.println(bbb.length()); //16 => 문자열의 길이
+        
+        //올바른 예
+        for(int q=0;q<strAr.length;q++){
+            for(int w=0;w<strAr[q].length();w++){
+                System.out.print(strAr[q].charAt(w)); //hong kim park
+            }
+            System.out.println();
+        }
+
+
     }
 }
